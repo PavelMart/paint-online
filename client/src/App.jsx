c93fef397cb684676bc5801aba76d0c0f0579695
PavelMart <martackovp@yandex.ru>
2023-02-07 21:16:50 +0300

    version 0.1

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./styles/app.scss";
+import SettingBar from "./components/SettingBar";
+import Toolbar from "./components/Toolbar";
+import Canvas from "./components/Canvas";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="app">
+                <Routes>
+                    <Route
+                        path="/:id"
+                        element={
+                            <>
+                                <Toolbar />
+                                <SettingBar />
+                                <Canvas />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="*"
+                        element={
+                            <Navigate to={`f${(+new Date()).toString(16)}`} />
+                        }
+                    />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default App;
